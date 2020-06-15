6b3448621cba172c4f17d898e19a650601bda1dd
Luiz Oliveira <luizoliveira@MacBook-Pro-de-Luiz.local>
2020-06-14 23:31:00 -0300

    Parte Mobile

---
+++ mobile/src/pages/Points/index.tsx
@@ -0,0 +1,150 @@
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, TouchableOpacity, Text, Image } from 'react-native';
+import Constants from 'expo-constants';
+import { Feather as Icon } from '@expo/vector-icons';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import MapView, { Marker } from 'react-native-maps';
+
+
+interface Point {
+    id: number;
+    name: string;
+    image: string;
+    image_url: string;
+    latitude: number;
+    longitude: number;
+  }
+
+const Points = () => {
+    const navigation = useNavigation();
+    const [points, setPoints] = useState<Point[]>([]);
+    const [initialPosition, setInitialPosition] = useState<[number, number]>([
+        0,
+        0,
+      ]);
+
+    function handleNavigateBack() {
+        navigation.goBack();
+    }
+
+    function handleNavigateToDetail(id: number) {
+        navigation.navigate('Detail', { point_id: id });
+      }
+
+    return ( 
+        <View style={styles.container}>
+            <TouchableOpacity onPress={handleNavigateBack}>
+                <Icon name="arrow-left" size={30} color="#34cb79" />
+            </TouchableOpacity> 
+
+            <Text style={styles.title}>Bem-vindo.</Text>
+            <Text style={styles.description}>
+            Encontre no mapa um ponto de coleta.
+            </Text>
+
+            <View style={styles.mapContainer}>
+                <MapView style={styles.map}/>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingHorizontal: 32,
+      paddingTop: 35 + Constants.statusBarHeight,
+    },
+  
+    title: {
+      fontSize: 20,
+      fontFamily: 'Ubuntu_700Bold',
+      marginTop: 24,
+    },
+  
+    description: {
+      color: '#6C6C80',
+      fontSize: 16,
+      marginTop: 4,
+      fontFamily: 'Roboto_400Regular',
+    },
+  
+    mapContainer: {
+      flex: 1,
+      width: '100%',
+      borderRadius: 10,
+      overflow: 'hidden',
+      marginTop: 16,
+    },
+  
+    map: {
+      width: '100%',
+      height: '100%',
+    },
+  
+    mapMarker: {
+      width: 90,
+      height: 80,
+    },
+  
+    mapMarkerContainer: {
+      width: 90,
+      height: 70,
+      backgroundColor: '#34CB79',
+      flexDirection: 'column',
+      borderRadius: 8,
+      overflow: 'hidden',
+      alignItems: 'center',
+    },
+  
+    mapMarkerImage: {
+      width: 90,
+      height: 45,
+      resizeMode: 'cover',
+    },
+  
+    mapMarkerTitle: {
+      flex: 1,
+      fontFamily: 'Roboto_400Regular',
+      color: '#FFF',
+      fontSize: 13,
+      lineHeight: 23,
+    },
+  
+    itemsContainer: {
+      flexDirection: 'row',
+      marginTop: 16,
+      marginBottom: 32,
+      paddingHorizontal: 32,
+    },
+  
+    item: {
+      backgroundColor: '#fff',
+      borderWidth: 2,
+      borderColor: '#eee',
+      height: 120,
+      width: 120,
+      borderRadius: 8,
+      paddingHorizontal: 16,
+      paddingTop: 20,
+      paddingBottom: 16,
+      marginRight: 8,
+      alignItems: 'center',
+      justifyContent: 'space-between',
+  
+      textAlign: 'center',
+    },
+  
+    selectedItem: {
+      borderColor: '#34CB79',
+      borderWidth: 2,
+    },
+  
+    itemTitle: {
+      fontFamily: 'Roboto_400Regular',
+      textAlign: 'center',
+      fontSize: 13,
+    },
+  });
+
+export default Points;
